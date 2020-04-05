80a1976fe4ffd0b119a49e78946231d1a1c52191
Gekkoisten <50629422+Gekkoisten@users.noreply.github.com>
2020-04-05 11:16:47 +0200

    Add files via upload

---
+++ hur.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#include "vector.hpp"        // vectors with components of type double
+#include "odeint.hpp"        // ODE integration routines, Runge-Kutta ...
+using namespace cpl;
+
+
+
+Vector f(const Vector& x) { 
+
+    return 0;
+}
+
+int main() {
+    
+}
+
